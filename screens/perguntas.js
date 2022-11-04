0041a2bb34a1251678751b1a7f768b60f04eafc4
Michel <106674127+MichelZero@users.noreply.github.com>
2022-11-04 08:56:02 -0300

    up

---
+++ screens/perguntas.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Perguntas = () => {
+  return (
+    <View>
+      <Text>Perguntas</Text>
+    </View>
+  )
+}
+
+export default Perguntas
+
+const styles = StyleSheet.create({})
